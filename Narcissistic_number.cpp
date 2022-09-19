e5308127366f9eaca8c26971509fc6ac9c413ce3
MoYanXG <113899037+MoYanXG@users.noreply.github.com>
2022-09-19 22:09:27 +0800

    水仙花数

---
+++ Narcissistic_number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int x, a, b, c, A, B, C;
+	cin >> x;
+	a = x / 100;
+	b = x % 100 / 10;
+	c = x % 10;
+	A = pow(a, 3);
+	B = pow(b, 3);
+	C = pow(c, 3);
+	if (A+B+C==x&&x>100)
+	{
+		cout << "YES" << endl;
+	}
+	else
+	{
+		cout << "NO" << endl;
+	}
+	return 0;
+}
