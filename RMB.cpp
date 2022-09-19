cc004fbbc0be42e9afc490622d45e79782574af4
MoYanXG <113899037+MoYanXG@users.noreply.github.com>
2022-09-19 22:02:31 +0800

    人民币兑换

---
+++ RMB.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x, ten, five, two, one;
+	cin >> x;
+	ten = x / 10;
+	five = x % 10 / 5;
+	two = x % 5 / 2;
+	one = x-ten*10-five*5-two*2;
+	cout << ten << " " << five << " " << two << " " << one << endl;
+	return 0;
+}
