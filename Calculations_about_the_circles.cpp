8df45d63b9e3fc9127aa0a034a0eff6422d89528
MoYanXG <113899037+MoYanXG@users.noreply.github.com>
2022-09-19 22:08:13 +0800

    圆相关计算

---
+++ Calculations_about_the_circles.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float r, h, p = 3.14;
+	double area, length, surface_area, volume;
+	cin >> r >> h;
+	area = p * r * r;
+	length = 2 * p * r;
+	surface_area = 4 * p * r * r;
+	volume = h * p * r * r;
+	cout << length << " " << area << " " << surface_area << " " << volume << " " << endl;
+	return 0;
+}
