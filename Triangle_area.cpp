00d6357662e5331f5b76056a16d7a7987b043459
MoYanXG <113899037+MoYanXG@users.noreply.github.com>
2022-09-19 22:05:40 +0800

    计算三角形面积

---
+++ Triangle_area.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	double s, a, b, c, m, k;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	s = (a + b + c) / 2;
+	k = (s - a) * (s - b) * (s - c) * s;
+	m = sqrt(k);
+	cout << m << endl;
+	return 0;
+}
